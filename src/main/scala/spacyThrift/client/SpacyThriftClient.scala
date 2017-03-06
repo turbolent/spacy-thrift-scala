d182b2ee344593244942151c7c330fa31b9a0793
Bastian Müller <bastian@turbolent.com>
2017-03-05 20:53:40 -0800

    use docker image for tests

--- src/main/scala/spacyThrift/client/SpacyThriftClient.scala
+++ src/main/scala/spacyThrift/client/SpacyThriftClient.scala
@@ -2,12 +2,20 @@ package spacyThrift.client
 
 import com.twitter.finagle.Thrift
 import com.twitter.finagle.Thrift.param.Framed
+import com.twitter.util.Future
 import spacyThrift.SpacyThrift.FinagledClient
+import spacyThrift.Token
 
 
 class SpacyThriftClient(hostname: String, port: Int) {
-  private val serviceFactory = Thrift.client.configured(Framed(false)).newClient(s"$hostname:$port")
-  private val client = new FinagledClient(serviceFactory.toService)
 
-  def tag(sentence: String) = client.tag(sentence)
+  private val client = {
+    val serviceFactory =
+      Thrift.client.configured(Framed(false))
+        .newClient(s"$hostname:$port")
+    new FinagledClient(serviceFactory.toService)
+  }
+
+  def tag(sentence: String): Future[Seq[Token]] =
+    client.tag(sentence)
 }
